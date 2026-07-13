69454d812397b2ec34ac9c868db52194e6651611
kawagh <kawagh.dev@gmail.com>
2026-07-14 07:16:25 +0900

    remove: terraform管理にしたいのでimportブロックを削除

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,11 +12,6 @@ resource "aws_s3_bucket" "site" {
   bucket = "terminal-prompt-customizer"
 }
 
-import {
-  to = aws_s3_bucket.site
-  id = "terminal-prompt-customizer"
-}
-
 # バケットへの直接の公開アクセスを全面ブロック(配信はCloudFront経由のみ)
 resource "aws_s3_bucket_public_access_block" "site" {
   bucket = aws_s3_bucket.site.id
@@ -27,22 +22,12 @@ resource "aws_s3_bucket_public_access_block" "site" {
   restrict_public_buckets = true
 }
 
-import {
-  to = aws_s3_bucket_public_access_block.site
-  id = "terminal-prompt-customizer"
-}
-
 resource "aws_cloudfront_origin_access_control" "site" {
   name                              = "oac-terminal-prompt-customizer.s3.ap-northeast-1.ama-mrhektuat4q"
   description                       = "Created by CloudFront"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
-}
-
-import {
-  to = aws_cloudfront_origin_access_control.site
-  id = "E2FQ0IX92W3P8O"
 }
 
 locals {
@@ -83,11 +68,6 @@ resource "aws_cloudfront_distribution" "site" {
   }
 }
 
-import {
-  to = aws_cloudfront_distribution.site
-  id = "E22IE47AH08JW3"
-}
-
 resource "aws_s3_bucket_policy" "site" {
   bucket = aws_s3_bucket.site.id
   policy = jsonencode({
@@ -110,9 +90,4 @@ resource "aws_s3_bucket_policy" "site" {
       }
     ]
   })
-}
-
-import {
-  to = aws_s3_bucket_policy.site
-  id = "terminal-prompt-customizer"
 }
