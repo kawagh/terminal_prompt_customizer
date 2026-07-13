9427eed792e928434d5b5dd61618ce28803b8898
kawagh <kawagh.dev@gmail.com>
2026-07-14 07:00:45 +0900

    add: CloudFrontディストリビューションをTerraform管理に取り込み

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,3 +44,42 @@ import {
   to = aws_cloudfront_origin_access_control.site
   id = "E2FQ0IX92W3P8O"
 }
+
+resource "aws_cloudfront_distribution" "site" {
+  enabled             = true
+  comment             = "terminal-prompt-customizerのDistribution"
+  default_root_object = "index.html"
+  is_ipv6_enabled     = true
+  tags = {
+    "Name" = "terminal-prompt-customizer Distribution"
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+  origin {
+    domain_name                 = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_access_control_id    = aws_cloudfront_origin_access_control.site.id
+    origin_id                   = "terminal-prompt-customizer.s3.ap-northeast-1.amazonaws.com-mrhei2h2naq"
+    origin_path                 = "/dist"
+    response_completion_timeout = 0
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  default_cache_behavior {
+    target_origin_id = "terminal-prompt-customizer.s3.ap-northeast-1.amazonaws.com-mrhei2h2naq"
+    allowed_methods  = ["GET", "HEAD"]
+    # https://docs.aws.amazon.com/ja_jp/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html#managed-cache-caching-optimized
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+}
+
+import {
+  to = aws_cloudfront_distribution.site
+  id = "E22IE47AH08JW3"
+}
